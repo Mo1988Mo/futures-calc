b158a56b37424c88ebe641e31c9529043d99811e
Mo1988Mo <mohsen.mohammadi.s@gmail.com>
2026-06-04 04:10:36 +0400

    add: React entry point src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import FuturesCalc from '../futures-trading-tool.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <FuturesCalc />
+  </React.StrictMode>
+)
